08277946e01d661bd24ab060f2809c1e704d54c6
chnirt <chnirt@users.noreply.github.com>
2024-05-05 09:59:56 +0000

    deploy: 011af2ca5c1db644f75a8680677e07a6a958ff3f

--- static/W_RYmuVgnYtJU7Oys5nD5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/ea7fcbde78ab9708.css","static/chunks/pages/index-9e42f2586eeb3294.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
